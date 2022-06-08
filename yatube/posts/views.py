8e577a7b6a967c441b61df596a9e0d5319ae8f73
Eugene Che <grem1in.che@yandex.ru>
2022-06-08 12:15:04 +0300

    added folderico to gitignore indent fix

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
-
 from yatube.settings import PAGINATOR_NUM_PAGE
 
 from .forms import CommentForm, PostForm
